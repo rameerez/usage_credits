b43f6e26b616e6513a1478c1187e7af51a7f3ca5
Javi R <4920956+rameerez@users.noreply.github.com>
2025-01-19 15:33:45 +0000

    Initial commit

---
+++ spec/usagecredits_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Usagecredits do
+  it "has a version number" do
+    expect(Usagecredits::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

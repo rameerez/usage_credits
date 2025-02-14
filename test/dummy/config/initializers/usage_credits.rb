069bd14026f4ebe82b5bbc3dc04e6a71cb398f89
Javi R <4920956+rameerez@users.noreply.github.com>
2025-02-14 02:17:33 +0000

    Simplify & improve demo app

--- test/dummy/config/initializers/usage_credits.rb
+++ test/dummy/config/initializers/usage_credits.rb
@@ -3,24 +3,20 @@
 UsageCredits.configure do |config|
 
   # Default test operations
-  operation :send_email do
+  operation :small_operation do
     costs 1.credit
   end
 
-  operation :test_operation do
-    costs 10.credits + 1.credits_per(:mb)
-    validate ->(params) { params[:size].to_i <= 100.megabytes }, "File too large (max: 100 MB)"
-  end
-
-  operation :expensive_operation do
-    costs 1_000.credits
-  end
+  # operation :test_operation do
+  #   costs 10.credits + 1.credits_per(:mb)
+  #   validate ->(params) { params[:size].to_i <= 100.megabytes }, "File too large (max: 100 MB)"
+  # end
 
   operation :prohibitive_operation do
     costs 1_000_000.credits
   end
 
-  operation :fail do
+  operation :this_operation_will_always_fail do
     costs 42.credits
   end
 
@@ -48,18 +44,17 @@ UsageCredits.configure do |config|
   subscription_plan :test_plan do
     processor_plan(:fake_processor, "abcdef123456")
     gives 10.credits.every(:month)
-    signup_bonus 5.credits
     unused_credits :expire
   end
 
   # Alert when balance drops below 100 credits
   # Set to nil to disable low balance alerts
-  config.low_balance_threshold = 20.credits
+  # config.low_balance_threshold = 20.credits
 
-  # Handle low credit balance alerts
-  config.on_low_balance do |user|
-    # Send notification to user when their balance drops below the threshold
-    ApplicationMailer.generic_email(to: user.email, body: "Heads up! You're low on credits.", subject: "Low credits alert").deliver_now
-  end
+  # # Handle low credit balance alerts
+  # config.on_low_balance do |user|
+  #   # Send notification to user when their balance drops below the threshold
+  #   ApplicationMailer.generic_email(to: user.email, body: "Heads up! You're low on credits.", subject: "Low credits alert").deliver_now
+  # end
 
 end
